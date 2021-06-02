3af47034ac203629145a8fd9a72f142389332882
Tino <67396655+tinocoppens@users.noreply.github.com>
2021-06-02 19:31:59 +0200

    Update discordwebhooksender.py

--- discordwebhooksender.py
+++ discordwebhooksender.py
@@ -1,31 +1,8 @@
-#you need
-#-python3
-#-requests (pip install requests)
-
-
-
-
-
-#import requests
 import requests
-
-#input by user
 discordwebhook = input("Discord Webhook : ")
 discordwebhookcontent = input("Content : ")
 discordwebhookusername = input("Username : ")
 discordwebhookpictureurl = input("Profile Picture URL : ")
-
-#define url
-url = discordwebhook
-
-#for post req
-data = {
-    "content" : discordwebhookcontent,
-    "username" : discordwebhookusername,
-    "avatar_url" : discordwebhookpictureurl
-}
-
-
-#send post request to discord api
-requests.post(url, data)
+data = {"content" : discordwebhookcontent, "username" : discordwebhookusername, "avatar_url" : discordwebhookpictureurl}
+requests.post(discordwebhook, data)
 
